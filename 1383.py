487a33728b68835a2c44350e6914f7fecb62c9e2
bodelha <bodelha@gmail.com>
2020-09-20 22:52:35 -0300

    1383 even better time

--- 1383.py
+++ 1383.py
@@ -1,21 +1,24 @@
-matrix = []
 out = []
 inst = 1
+matrix = ''
 
-def confere (lista):
-    for i in range (9):
-        quadrante = lista[3*(i//3)][3*(i%3):3*(i%3) + 3] + lista[3*(i//3)+1][3*(i%3):3*(i%3) + 3] + lista[3*(i//3)+2][3*(i%3):3*(i%3) + 3]
+def confere (string):
+    for i in range(9):
+        linha = string [i*9 : i*9 + 9]
+        coluna = string [i::9]
+        index = 27*(i//3) + 3*(i%3)
+        quadrante = string[index: index + 3] + string[index + 9:index + 12] + string[index + 18:index + 21]
         for n in range (1, 9):
-            if lista [i].count(n) != 1 or [lista[k][i] for k in range(9)].count(n) !=1 or quadrante.count(n) != 1:
+            if linha.count(str(n)) !=1 or coluna.count(str(n)) !=1 or quadrante.count(str(n)) !=1:
                 return False
     return True
 
 for _ in range (int(input())*9):
-    matrix.append (list(map(int, input().split())))
-    if len(matrix) == 9:
+    matrix += input().replace(" ", "")
+    if len(matrix) == 81:
         out.append("Instancia " + str(inst))
         out.append("SIM\n" if confere(matrix) else "NAO\n")
-        matrix = []
+        matrix = ''
         inst += 1
 
 print(*out, sep='\n')
