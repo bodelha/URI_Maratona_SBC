20aeddf5f0037d368a36c684da44dfe207643620
bodelha <bodelha@gmail.com>
2020-09-20 22:15:53 -0300

    1383 better time

--- 1383.py
+++ 1383.py
@@ -3,12 +3,10 @@ out = []
 inst = 1
 
 def confere (lista):
-    for n in range (1, 9):
-        for i in range (9):
-            linha = lista [i]
-            coluna = [lista[k][i] for k in range(9)]
-            quadrante = lista[3*(i//3)][3*(i%3):3*(i%3) + 3] + lista[3*(i//3)+1][3*(i%3):3*(i%3) + 3] + lista[3*(i//3)+2][3*(i%3):3*(i%3) + 3]
-            if linha.count(n) != 1 or coluna.count(n) !=1 or quadrante.count(n) != 1:
+    for i in range (9):
+        quadrante = lista[3*(i//3)][3*(i%3):3*(i%3) + 3] + lista[3*(i//3)+1][3*(i%3):3*(i%3) + 3] + lista[3*(i//3)+2][3*(i%3):3*(i%3) + 3]
+        for n in range (1, 9):
+            if lista [i].count(n) != 1 or [lista[k][i] for k in range(9)].count(n) !=1 or quadrante.count(n) != 1:
                 return False
     return True
 
@@ -16,10 +14,8 @@ for _ in range (int(input())*9):
     matrix.append (list(map(int, input().split())))
     if len(matrix) == 9:
         out.append("Instancia " + str(inst))
-        out.append("SIM" if confere(matrix) else "NAO")
-        out.append("")
+        out.append("SIM\n" if confere(matrix) else "NAO\n")
         matrix = []
         inst += 1
 
 print(*out, sep='\n')
-
