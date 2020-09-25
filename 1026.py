9e97209f1387b4792120d4b7eef1647ebc392884
bodelha <bodelha@gmail.com>
2020-09-24 22:50:47 -0300

    1026 v3

--- 1026.py
+++ 1026.py
@@ -1,10 +1,14 @@
 out = []
 while True:
     try:
-        a, b = map(int, input().split())
-        a, b = int(bin(a).replace("0b", "")), int(bin(b).replace("0b", ""))
-        out.append (int ("0b"+(str(a+b)).replace("2", "0"), 2))
-        
+        a, b = map (lambda x: bin(int(x)), input().split())
+        if len (a) < len (b):
+            a, b = b, a
+        resp = ''
+        for i in range (len(b) - 1, 1, -1):
+            soma = int(a[len(a) -len (b) + i]) + int(b[i])
+            resp =  '1' + resp if soma == 1 else '0' + resp
+        out.append(int ("0b" + a[2:2 + len(a)-len(b)] + resp, 2))
     except EOFError:
         break
 print(*out, sep='\n')
