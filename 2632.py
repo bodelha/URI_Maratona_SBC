348b8d4921e126694a6625c8d7dc168c5f56265a
bodelha <bodelha@gmail.com>
2020-11-02 15:48:01 -0300

    better 2632

--- 2632.py
+++ 2632.py
@@ -5,30 +5,30 @@ radius = {
     "earth": (400, 25, 55, 70),
     "air": (100, 18, 38, 60)
 }
-n = int(input())
 
-for _ in range(n):
+for n in range(int(input())):
     w, h, x0, y0 = map(int, input().split())
+    xw, yh = x0 + w, y0 +h
     [magic, level, cx, cy] = input().split()
     level, cx, cy = int(level), int (cx), int (cy)
     points =[
-        (x0, y0), (x0, y0 + h), (x0 + w, y0 + h), (x0 + w, y0)
+        (x0, y0), (x0, yh), (xw, yh), (xw, y0)
     ]
-    if cx >= points[0][0] and cx <= points [2][0]:
-        points.append((cx, y0))
-        points.append((cx, y0 + h))
-    if cy >= points[0][1] and cy <= points[1][1]:
+    if x0 <= cx and cx <= xw:
+        if y0 <= cy and cy <= yh:
+            points = [(cx, cy)]
+        else:
+            points.append((cx, y0))
+            points.append((cx, yh))
+    elif y0 <= cy and cy <= yh:
         points.append((x0, cy))
-        points.append((x0 + w, cy))
-    distances = []
+        points.append((xw, cy))    
     for point in points:
-        distances.append(((point[0] - cx)**2+(point[1] - cy)**2)**0.5)
-    if min(distances) <= radius[magic][level] or (
-        points [0][0] <= cx and cx <= points [-1][0] and
-        points [0][1]<= cy and cy <= points [1][1] 
-    ):
-        out.append(radius[magic][0])
-    else:
-        out.append (0)
+        distance = ((point[0] - cx)**2+(point[1] - cy)**2)**0.5 
+        if distance <= radius[magic][level]:
+            out.append(radius[magic][0])
+            break
+    if len(out) != n + 1:
+            out.append (0)
 
 print (*out, sep="\n")
